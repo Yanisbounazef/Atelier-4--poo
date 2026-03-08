176ffce05dac95d7793d9433634fdf2763505847
Yanis <yanis.bounazef@monboreal.ca>
2026-03-08 17:47:00 -0400

    creation de la classe voiture

--- main.py
+++ main.py
@@ -30,3 +30,10 @@ class Employe:
                 if self.voitureService != None:
                     self.voitureService.chauffeur = None
                     self.voitureService = None
+class Voiture:
+    def __init__(self, matricule, annee, marque, kilometrage):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
